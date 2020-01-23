dcdf6aaf42c374c17a844371630658b51b4fdb5c
Dion Garman <diongarman@gmail.com>
2020-01-23 21:57:33 +0000

    closures example 2

--- closures.js
+++ closures.js
@@ -11,4 +11,38 @@ const x = () => {
     }
 }
 
-console.log(x().returnX());
+//x().returnX()
+
+
+const counter = () => {
+
+    let i = 0;
+
+
+    const add = (value) => {
+        i+= value;
+        
+    };
+
+    const subtract = (value) => {
+        i+= value;
+    };
+
+
+    const print = () => {
+
+        console.log(i)
+    };
+
+
+    return {
+
+        add,
+        subtract,
+        print
+    }
+}
+
+let outerCounter = counter();
+outerCounter.add(3);
+outerCounter.print();
